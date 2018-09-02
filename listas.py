27a31be86581feaa3f1bdacec8131693f5154295
Gabyluu <42919324+Gabyluu@users.noreply.github.com>
2018-09-02 16:23:01 -0700

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,24 @@
+#listas py
+
+#listas o array
+
+l = [54, 12, 67, 23]
+
+# print(l)
+
+# l.append(65)
+
+# l.insert(0,7) 
+
+
+
+#print(l[0])
+#print(l[2])
+
+# for item in l:
+# 	print(item)
+
+# del (l[1])
+
+print(sorted(l))
+print(l)
---
+++ tiposDatos.py
@@ -0,0 +1,11 @@
+
+ # Integers
+
+a = 34
+
+b = 76
+
+print(a + b)
+
+print(a / b)
+print(a // b)
